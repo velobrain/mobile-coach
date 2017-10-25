998b107943d2da22cc6b30b678c74b26f6f9419e
Nicolas <nicolasspragg@gmail.com>
2017-10-25 17:14:38 -0400

    added in text field properties and keyboard dismissing.

--- VeloBrain/VeloBrain/WorkOutController.swift
+++ VeloBrain/VeloBrain/WorkOutController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class WorkOutController: UIViewController {
     
+    @IBOutlet var speedTextField: UITextField!
+    @IBOutlet var timeTextField: UITextField!
+    @IBOutlet var heartRateLowField: UITextField!
+    @IBOutlet var heartRateHighField: UITextField!
+    
     override func viewDidLoad() {
-     
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
