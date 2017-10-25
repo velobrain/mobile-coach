6ab4baea12bdb005aa3148fdc7b85403ae2c93a2
Nicolas <nicolasspragg@gmail.com>
2017-10-25 16:18:20 -0400

    removed pickers.

--- VeloBrain/VeloBrain/WorkOutController.swift
+++ VeloBrain/VeloBrain/WorkOutController.swift
@@ -8,18 +8,10 @@
 
 import UIKit
 
-class WorkOutController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-   
-    @IBOutlet weak var heartRateLowPicker: UIPickerView!
-    var pickerDataLow: [String] = [String]()
-    
+class WorkOutController: UIViewController {
     
     override func viewDidLoad() {
-        pickerDataLow = ["90","100","110"]
-        heartRateLowPicker.delegate = self
-        heartRateLowPicker.dataSource = self
-        
-      
+     
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -29,17 +21,6 @@ class WorkOutController: UIViewController, UIPickerViewDelegate, UIPickerViewDat
         // Dispose of any resources that can be recreated.
     }
     
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerDataLow.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return pickerDataLow[row]
-    }
-    
+
+        
 }
