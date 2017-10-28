9b39afb366cbef29d78a1ea3ad601ec4e1e6c030
Nicolas <nicolasspragg@gmail.com>
2017-10-28 01:12:41 -0400

    updated the segues. Started error checking on inputs. Need to fix error with optional values

--- VeloBrain/VeloBrain/WorkOutController.swift
+++ VeloBrain/VeloBrain/WorkOutController.swift
@@ -10,18 +10,73 @@ import UIKit
 
 class WorkOutController: UIViewController {
     
+    enum workOutParamErrors: Error {
+        case tooFast // 35m/s
+        case tooSlow // 3m/s
+        case tooSlowHB //50
+        case tooFastHB //180
+        case emptyFast
+        case emptySlow
+        case emptyFastHB
+        case emptySlowHB
+    }
+    
+    
+    //mark instance variables
     @IBOutlet var speedTextField: UITextField!
-    @IBOutlet var timeTextField: UITextField!
-    @IBOutlet var heartRateLowField: UITextField!
-    @IBOutlet var heartRateHighField: UITextField!
+    
+   
+    @IBAction func startWorkoutButton(_ sender: Any) {
+        textInputTest()
+        
+        /*
+        do{
+            try getSpeedUtil()
+        } catch workOutParamErrors.tooSlow {
+            print("speed is too slow < 5")
+        } catch workOutParamErrors.tooFast {
+            print("speed is too fast > 35")
+        } catch workOutParamErrors.emptyFast{
+            print("You did not enter a speed")
+        } catch let randomError {
+            print("??????",randomError)
+        }
+         */
+    }
+    //mark instance variables
     
     override func viewDidLoad() {
-        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
-        tap.cancelsTouchesInView = false
-        self.view.addGestureRecognizer(tap)
+      removeKB()
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    //removes the keyboard by tapping anywhere on screen
+    func removeKB() {
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+    }
+    
+    func getSpeedUtil() throws{
+        guard let speed = speedTextField.text else {
+            throw workOutParamErrors.emptyFast
+        }
+        guard speed > "35" else {
+            throw workOutParamErrors.tooFast
+        }
+        guard speed < "5" else {
+            throw workOutParamErrors.tooSlow
+        }
+    }
+    
+    //Fix this
+    func textInputTest() {
+        
+        print(self.speedTextField.text!)
+        
+    }
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
